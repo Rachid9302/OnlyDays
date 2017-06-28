0cabd404acecd16fc13ad17585ceb4677a03a711
Rachid9302 <rachid.elamirchi@gmail.com>
2017-06-28 18:14:16 +0200

    commit 3

--- src/AppBundle/Form/AnnonceType.php
+++ src/AppBundle/Form/AnnonceType.php
@@ -3,8 +3,11 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AnnonceType extends AbstractType
 {
@@ -13,23 +16,40 @@ class AnnonceType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('reference')
-            ->add('titre')
+        $builder->add('titre')
             ->add('description')
             ->add('superficie')
             ->add('piece')
             ->add('chambre')
             ->add('lit')
             ->add('cuisine')
-            ->add('voyageurmax')
+            ->add('voyageurmax', TextType::class, array(
+                'label' => "Voyageur maximum"
+            ))
             ->add('wifi')
-            ->add('prixnuit')
-            ->add('dateDepot')
+            ->add('prixnuit', TextType::class, array(
+                'label' => "Prix nuit"
+            ))
+            ->add('dateDepot', HiddenType::class )
             ->add('firstimage')
-            ->add('typepropriete')
-            ->add('auteur')
-            ->add('last_auteur')
-            ->add('arrondissement');
+            ->add('typepropriete', TextType::class, array(
+                'label' => "Type de propriété"
+            ))
+            ->add('auteur', HiddenType::class )
+            ->add('last_auteur', HiddenType::class)
+            ->add('arrondissement', ChoiceType::class, array(
+                'choices' => array(
+                    '1 arrondissement' => '1',
+                    '2 arrondissement' => '2',
+                    '3 arrondissement' => '3',
+                    '4 arrondissement' => '4',
+                    '5 arrondissement' => '5',
+                    '6 arrondissement' => '6',
+                    '7 arrondissement' => '7',
+                    '8 arrondissement' => '8',
+                    '9 arrondissement' => '9',
+                )
+            ));
     }
     
     /**
